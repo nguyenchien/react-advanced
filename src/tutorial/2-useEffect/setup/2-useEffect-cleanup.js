011cd785ff287b154f53aef32ce14db44c30264b
chienvn <chiennguyen1702@gmail.com>
2022-10-09 21:45:21 +0700

    useEffect - cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,30 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
+  
+  useEffect(()=>{
+    console.log('useEffect render');
+    window.addEventListener('resize', checkSize);
+    
+    return () => {
+      console.log('cleanup function');
+      window.removeEventListener('resize', checkSize);
+    }
+    // Or use 'Dependency List is: []'
+  });
+  
+  console.log('component render');
+  return (
+    <>
+      <h1>{size}</h1>
+      <h2>PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
