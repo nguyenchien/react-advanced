d4c9c93979bcdbfa838a327f8ccf691fda4ec042
chienvn <chiennguyen1702@gmail.com>
2022-10-09 20:31:00 +0700

    useEffect - Basic

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,21 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+// *** Note in mind: Hook not in condition
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  
+  useEffect(()=> {
+    if (value >= 1 ) {
+      document.title = `useEffect render ${value}`;
+    }
+  });
+  
+  const [value, setValue] = useState(0);
+  console.log('component render');
+  return <>
+    <h2>{value}</h2>
+    <button className='btn' onClick={()=>setValue(value + 1)}>click me</button>
+  </>;
 };
 
 export default UseEffectBasics;
