8f00ef14e277f5ab84711e52f8b7d7123e3e93db
chienvn <chiennguyen1702@gmail.com>
2022-10-11 06:02:14 +0700

    useEffect - fetch data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
-const url = 'https://api.github.com/users';
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+// second argument
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  
+  const [users, setUsers] = useState([]);
+  
+  const getUsers = async() => {
+    const respond =  await fetch(url);
+    const users = await respond.json();
+    setUsers(users);
+  }
+  
+  useEffect(()=>{
+    getUsers();
+  }, []); 
+  // Dependency List is verry very important.
+  // Careful with infinite loop
+  
+  return <>
+    <h2>Users List</h2>
+    <ul className='users'>
+      {users && users.length > 0 && users.map((user)=>{
+        const {id, name, username, email} = user;
+        return (
+          <li key={id}>
+            <div>
+              <h4>{name}</h4>
+              <a href='#'>{email}</a>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  </>;
 };
 
 export default UseEffectFetchData;
