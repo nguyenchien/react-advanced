3b6ae91256c1be409346dcffc77e39bee314e802
chienvn <chiennguyen1702@gmail.com>
2022-11-20 17:23:00 +0700

    useReducer: setup

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,44 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      setPeople([
+        ...people,
+        {
+          id: new Date().getTime().toString(),
+          name
+        }
+      ]);
+      setName('');
+      setShowModal(true);
+    } else {
+      setShowModal(true);
+    }
+  }
+  
+  
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {people.map((person)=>{
+        return (
+          <div className="item" key={person.id}>{person.name}</div>
+        )
+      })}
+    </>
+  );
 };
 
 export default Index;
