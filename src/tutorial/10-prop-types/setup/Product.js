abf66bab569d5ea0c2b25e94825b0b66dd4984a1
chienvn <chiennguyen1702@gmail.com>
2022-11-23 21:07:19 +0700

    PropTypes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image, name, price}) => {
+  const url = image && image.url;
+  const defaultImage = 'https://via.placeholder.com/400';
+  return (
+    // <article className='product'>
+    //   { image && image.url ? <img src={image.url} alt={name} /> : <img src='https://via.placeholder.com/400' alt={name} />}
+    //   <h4>{name && name}</h4>
+    //   <p>${price && price}</p>
+    // </article>
+    
+    // use prop type
+    <article className='product'>
+      <img src={url || defaultImage} alt={name || 'default image'} />
+      <h4>{name || 'default name'}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+
+// Product.defaultProps = {
+//   image: <img src='https://via.placeholder.com/400' alt='' />,
+//   name: 'default name',
+//   price: 3.99
+// };
 
 export default Product;
