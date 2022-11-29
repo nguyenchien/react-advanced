75886a33fe5cf3bf00115c464e4ef8a6e7986527
chienvn <chiennguyen1702@gmail.com>
2022-11-29 21:17:26 +0700

    useMemo, useCallback

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -7,9 +7,31 @@ const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
 
+const calculateMostExpensive = (data) => {
+  console.log('hello');
+  return (
+    data.reduce((total, item)=>{
+      const price = item.fields.price;
+      if (price >= total) {
+        total = price;
+      }
+      return total;
+    }, 0) / 100
+  )
+}
+
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+  const [cart, setCart] = useState(0)
+  
+  const addToCart = useCallback(() => {
+    setCart(cart + 1)
+  }, [cart]);
+  
+  const mostExpensive = useMemo(() => calculateMostExpensive(products),[
+    products
+  ])
 
   return (
     <>
@@ -17,22 +39,26 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{padding: '3rem'}}>cart : {cart}</h1>
+      <h1>most expensive : ${mostExpensive}</h1>
+      <BigList products={products} addToCart = {addToCart} />
     </>
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
+  console.log('big list called');
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>
       })}
     </section>
   )
-}
+})
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
+  console.count('single product called');
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,6 +68,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   )
 }
